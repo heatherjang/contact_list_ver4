d64011e74aef4b818a46aedc0eba146b594352d4
heatherjang <heather.m.jang@gmail.com>
2015-03-11 00:24:43 +0000

    Index displays all contacts on button click

--- app/actions.rb
+++ app/actions.rb
@@ -4,6 +4,27 @@ get '/' do
 end
 
 get '/contacts' do
-  @contacts = Contact.all
-  erb :'contacts/index'
+  @contacts = Contact.all.to_json
+  # erb :'contacts/index'
+end
+
+
+
+
+
+get '/contacts/new' do
+  erb :'contacts/new'
+end
+
+post '/contacts' do
+  contact = Contact.create(
+    email: params[:email],
+    firstname: params[:firstname],
+    lastname: params[:lastname],
+  )
+  if @contact.save
+    redirect '/contacts'
+  else
+    erb :'contacts/new'
+  end
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,23 @@
-$(document).ready(function() {
+$(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var allContactsButton = $('#show_contacts');
+
+  allContactsButton.on('click', function() {
+    $.ajax({
+        method: 'GET',
+        url: '/contacts', 
+        success: displayAllContacts, 
+        dataType: 'json',
+        contentType: 'application/json;charset=utf-8'
+    });
+});
+
+  function displayAllContacts(contacts) {
+    $.each(contacts, function(index, contact) {
+      var contactP = $('<p>');
+      contactP.html(contact['firstname'] + " " + contact['lastname'] + " (" + contact['email'] + ")");
+      $(document.body).append(contactP)
+    });
+  }
+
 });
